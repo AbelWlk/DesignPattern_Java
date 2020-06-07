1f0b67595d8bd8d9bbb9498db55eb490e7cd45de
abelwlk <abelwlk@foxmail.com>
2020-06-07 22:20:45 +0800

    工厂方法模式_反射优化

--- CreationalPattern/src/FactoryMethodPattern/Client.java
+++ CreationalPattern/src/FactoryMethodPattern/Client.java
@@ -1,13 +1,31 @@
 package FactoryMethodPattern;
 
+import FactoryMethodPattern.utils.XMLUtil;
+
+import javax.jws.soap.SOAPBinding;
+
 /**
  * 客户端测试代码
  */
 public class Client {
     public static void main(String[] args) {
+        UseClass();
+        UseReflection();
+    }
+
+    public static void UseClass() {
         LoggerFactory factory;
         Logger logger;
         factory = new FileLoggerFactory();
+        logger = factory.createLogger();
+        logger.writeLog();
+    }
+
+    public static void UseReflection() {
+        LoggerFactory factory;
+        Logger logger;
+        factory = (LoggerFactory) XMLUtil.getBean();
+        assert factory != null;
         logger = factory.createLogger();
         logger.writeLog();
     }
---
+++ CreationalPattern/src/FactoryMethodPattern/utils/XMLUtil.java
@@ -0,0 +1,29 @@
+package FactoryMethodPattern.utils;
+
+import javax.xml.parsers.*;
+import org.w3c.dom.*;
+import java.io.*;
+
+public class XMLUtil {
+    //该方法用于从XML配置文件中提取具体类类名，并返回一个实例对象
+    public static Object getBean() {
+        try {
+            //创建DOM文档对象
+            DocumentBuilderFactory dFactory = DocumentBuilderFactory.newInstance();
+            DocumentBuilder builder = dFactory.newDocumentBuilder();
+            Document doc;
+            doc = builder.parse(new File("CreationalPattern/src/config.xml"));
+
+            //获取包含类名的文本节点
+            NodeList nl = doc.getElementsByTagName("className");
+            Node classNode = nl.item(0).getFirstChild();
+            String cName = classNode.getNodeValue();
+
+            //通过类名生成实例对象并将其返回
+            return Class.forName(cName).newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+}
